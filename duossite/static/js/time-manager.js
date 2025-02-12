162551f957e85fa680f7b6d967e6f4c05f8d0fda
Thomas Retzloff <tretzloff@gmail.com>
2025-02-12 16:30:32 -0500

    fixed interval size https://smashlegends.com/2-11tue-update-notes_en/

--- duossite/static/js/time-manager.js
+++ duossite/static/js/time-manager.js
@@ -1,7 +1,7 @@
 let timeManager = {
     answerMap : {true: 'yes', false: 'no'},
     iconPaths : {true: '/static/media/images/yes.svg', false: '/static/media/images/no.svg'},
-    intervalSize : 5,
+    intervalSize : 2.5,
     startTimeStr : "2024-12-31T17:00:00Z",
     startTime : null,
     timeLeftClass : 'time-left',
